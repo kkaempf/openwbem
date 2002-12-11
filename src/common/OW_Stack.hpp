97843131aaba7ec29ebe2ce515137dc060dc6881
Dan Nuffer <nuffer>
2002-12-11 04:34:51 +0000

    Greatly improved.

--- src/common/OW_Stack.hpp
+++ src/common/OW_Stack.hpp
@@ -6,23 +6,34 @@
 #include "OW_Array.hpp"
 
 template<class T>
-class OW_Stack : public OW_Array<T>
+class OW_Stack : private OW_Array<T>
 {
 public:
+
+	typedef typename OW_Array<T>::size_type size_type;
+	typedef typename OW_Array<T>::reference reference;
+	typedef typename OW_Array<T>::const_reference const_reference;
+
 	OW_Stack() : OW_Array<T>() {  }
 
-	bool empty() {   return (size() == 0); }
+	bool empty() const {   return (size() == 0); }
 
-	T peek() {   return operator[](size()-1); }
+	reference top() {   return operator[](size()-1); }
+	const_reference top() const {   return operator[](size()-1); }
 
-	T pop() {   T obj = peek(); remove(size()-1); return obj; }
+	void pop() { remove(size()-1); }
 
-	T push(const T& x) {   push_back(x); return x; }
+	void push(const T& x) {   push_back(x); }
 
-	int search(const T& x)
+	int search(const T& x) const
 	{
 		int i = find(x);
 		return (i >= 0) ? int(size()) - i : -1;
+	}
+
+	size_type size() const
+	{
+		return OW_Array<T>::size();
 	}
 };
 
