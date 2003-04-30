2c162ed9126ca6908a3632b4b31242dd0b2412a1
Dan Nuffer <nuffer>
2003-04-30 18:07:13 +0000

    Added a cassert include.

--- src/cim/OW_CIMValue.cpp
+++ src/cim/OW_CIMValue.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2001 Center 7, Inc All rights reserved.
+* Copyright (C) 2001-3 Center 7, Inc All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -37,6 +37,7 @@
 #include "OW_CIMValueCast.hpp" // for OW_ValueCastException
 
 #include <new>
+#include <cassert>
 
 using std::istream;
 using std::ostream;
