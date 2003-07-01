361f55a2e20733864a0f7b3e11c5644cb45a620f
Dan Nuffer <nuffer>
2003-07-01 21:06:46 +0000

    Added the DISABLE_DIGEST macro

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2001 Center 7, Inc All rights reserved.
+* Copyright (C) 2001-3 Center 7, Inc All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -87,6 +87,7 @@
 
 #undef DISABLE_ACLS
 #undef ENABLE_PROPERTY_PROVIDERS
+#undef DISABLE_DIGEST
 
 @BOTTOM@
 
