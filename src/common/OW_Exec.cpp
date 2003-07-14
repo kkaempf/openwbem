4842e39defba5c566b1f30bcb4bd07adaa28c563
Dan Nuffer <nuffer>
2003-07-14 21:39:37 +0000

    Wrote executeProcessAndGatherOutput, gatherOutput, and getExitStatus. Added javadoc.

--- src/common/OW_Exec.cpp
+++ src/common/OW_Exec.cpp
@@ -35,6 +35,8 @@
 #include "OW_PosixUnnamedPipe.hpp"
 #include "OW_Array.hpp"
 #include "OW_IOException.hpp"
+#include "OW_Thread.hpp"
+#include "OW_Select.hpp"
 
 extern "C"
 {
@@ -42,6 +44,7 @@ extern "C"
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <stdio.h> // for perror
 
 #ifdef OW_USE_GNU_PTH
 #include <pth.h>
@@ -54,6 +57,10 @@ using std::cerr;
 using std::endl;
 
 typedef OW_Reference<OW_PosixUnnamedPipe> OW_PosixUnnamedPipeRef;
+
+DEFINE_EXCEPTION(ExecTimeout);
+DEFINE_EXCEPTION(ExecBufferFull);
+DEFINE_EXCEPTION(ExecError);
 
 //////////////////////////////////////////////////////////////////////////////
 int
@@ -95,7 +102,7 @@ OW_Exec::safeSystem(const OW_Array<OW_String>& command)
 		}
 		argv[command.size()] = 0;
 		int rval = execv(argv[0], argv);
-		cerr << format( "OW_Platform::safePopen: execv failed for program "
+		cerr << format( "OW_Platform::safeSystem: execv failed for program "
 				"%1, rval is %2", argv[0], rval);
 		_exit(1);
 	}
@@ -121,24 +128,43 @@ OW_Exec::safeSystem(const OW_Array<OW_String>& command)
 
 class OW_PopenStreamsImpl
 {
-	public:
-		OW_UnnamedPipeRef in();
-		void in(OW_UnnamedPipeRef pipe);
-		OW_UnnamedPipeRef out();
-		void out(OW_UnnamedPipeRef pipe);
-		OW_UnnamedPipeRef err();
-		void err(OW_UnnamedPipeRef pipe);
-		pid_t pid();
-		void pid(pid_t newPid);
-		int getExitStatus();
-		~OW_PopenStreamsImpl();
+public:
+	OW_PopenStreamsImpl();
+	~OW_PopenStreamsImpl();
 
-	private:
-		OW_UnnamedPipeRef m_in;
-		OW_UnnamedPipeRef m_out;
-		OW_UnnamedPipeRef m_err;
-		pid_t m_pid;
+	OW_UnnamedPipeRef in();
+	void in(OW_UnnamedPipeRef pipe);
+
+	OW_UnnamedPipeRef out();
+	void out(OW_UnnamedPipeRef pipe);
+
+	OW_UnnamedPipeRef err();
+	void err(OW_UnnamedPipeRef pipe);
+
+	pid_t pid();
+	void pid(pid_t newPid);
+
+	int getExitStatus();
+
+	void setProcessStatus(int ps)
+	{
+		m_processstatus = ps;
+	}
+
+private:
+	OW_UnnamedPipeRef m_in;
+	OW_UnnamedPipeRef m_out;
+	OW_UnnamedPipeRef m_err;
+	pid_t m_pid;
+	int m_processstatus;
 };
+
+//////////////////////////////////////////////////////////////////////////////
+OW_PopenStreamsImpl::OW_PopenStreamsImpl()
+	: m_pid(0)
+	, m_processstatus(-1)
+{
+}
 
 //////////////////////////////////////////////////////////////////////////////
 OW_UnnamedPipeRef OW_PopenStreamsImpl::in()
@@ -183,39 +209,149 @@ void OW_PopenStreamsImpl::pid(pid_t newPid)
 	m_pid = newPid;
 }
 
+//////////////////////////////////////////////////////////////////////
+static inline pid_t lwaitpid(pid_t pid, int* status, int options)
+{
+#ifdef OW_USE_PTH
+	return pth_waitpid(pid, status, options);
+#else
+	return ::waitpid(pid, status, options);
+#endif
+}
+
+//////////////////////////////////////////////////////////////////////
+static pid_t
+waitpidNoINTR(pid_t pid, int* status, int options)
+{
+	pid_t waitpidrv;
+	do
+	{
+		waitpidrv = lwaitpid(pid, status, options);
+	} while (waitpidrv == -1 && errno == EINTR);
+	return waitpidrv;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+static inline void
+milliSleep(OW_UInt32 milliSeconds)
+{
+	OW_Thread::sleep(milliSeconds);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+static inline void
+secSleep(OW_UInt32 seconds)
+{
+	OW_Thread::sleep(seconds * 1000);
+}
+
 //////////////////////////////////////////////////////////////////////////////
 int OW_PopenStreamsImpl::getExitStatus()
 {
-	int status;
-	do
+	// Close the streams. If the child process is blocked waiting to output,
+	// then this will cause it to get a SIGPIPE, and it may be able to clean
+	// up after itself.
+	in()->close();
+	out()->close();
+	err()->close();
+
+	// Now make sure the process has exited. We do everything possible to make 
+	// sure the sub-process dies.
+	if (m_pid != -1) // it's set to -1 if we already sucessfully waited for it.
 	{
-#ifdef OW_USE_GNUPTH
-		if (pth_waitpid(m_pid, &status, 0) == -1)
-#else
-		if (waitpid(m_pid, &status, 0) == -1)
-#endif
+		pid_t waitpidrv;
+		waitpidrv = waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+		
+		// give it 10 seconds to quit
+		if (waitpidrv == 0)
 		{
-			if (errno != EINTR)
-				return -1;
+			for (int i = 0; i < 100 && waitpidrv == 0; ++i)
+			{
+				milliSleep(100); // 1/10 of a second
+				waitpidrv = waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+			}
+		}
+
+		if (waitpidrv == 0)
+		{
+			if (kill(m_pid, SIGTERM) != -1)
+			{
+				milliSleep(10);
+	
+				waitpidrv = waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+				
+				if (waitpidrv == 0)
+				{
+					secSleep(3);
+
+					waitpidrv = waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+				}
+
+				if (waitpidrv == 0)
+				{
+					/* process still didn't terminate after a SIGTERM, so we'll
+					   try sending it SIGKILL a few times */
+					for (int i = 0; i < 10; ++i)
+					{
+						if (kill(m_pid, SIGKILL) == -1)
+						{
+							// call waitpid in case the thing has turned into a zombie.
+							waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+							OW_THROW(OW_ExecErrorException, format("Failed sending SIGKILL to process %1. errno = %2(%3)\n", m_pid, errno, strerror(errno)).c_str());
+						}
+
+						milliSleep(50);
+
+						waitpidrv = waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+						if (waitpidrv == 0)
+						{
+							OW_THROW(OW_ExecErrorException, format("Child process has not exited after sending it a SIGKILL. errno = %1(%2)\n", errno, strerror(errno)).c_str());
+						}
+						else
+						{
+							break;
+						}
+					}
+				}
+				else if (waitpidrv > 0)
+				{
+					m_pid = -1;
+				}
+				else
+				{
+					OW_THROW(OW_ExecErrorException, format("waitpid failed.  errno = %1(%2)\n", errno, strerror(errno)).c_str());
+				}
+			}
+			else
+			{
+				// call waitpid in case the thing has turned into a zombie.
+				waitpidNoINTR(m_pid, &m_processstatus, WNOHANG);
+				OW_THROW(OW_ExecErrorException, format("Failed sending SIGTERM to process %1. errno = %2(%3)\n", m_pid, errno, strerror(errno)).c_str());
+			}
+		}
+		else if (waitpidrv > 0)
+		{
+			m_pid = -1;
 		}
 		else
 		{
-			return WEXITSTATUS(status);
+			OW_THROW(OW_ExecErrorException, format("waitpid failed.  errno = %1(%2)\n", errno, strerror(errno)).c_str());
 		}
-	} while (1);
+	}
+	return m_processstatus;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 OW_PopenStreamsImpl::~OW_PopenStreamsImpl()
 {
-	// prevent zombie children
-	::kill(pid(), SIGTERM);
-	in()->close();
-	out()->readAll();
-	out()->close();
-	err()->readAll();
-	err()->close();
-	getExitStatus();
+	try // can't let exceptions past.
+	{
+		// This will terminate the process.
+		getExitStatus();
+	}
+	catch (...)
+	{
+	}
 }
 
 
@@ -243,8 +379,7 @@ OW_Exec::safePopen(const OW_Array<OW_String>& command,
 
 	if (command.size() == 0)
 	{
-		retval.pid( -1 );
-		return retval;
+		OW_THROW(OW_ExecErrorException, "command is empty");
 	}
 
 #ifdef OW_USE_GNU_PTH
@@ -254,7 +389,7 @@ OW_Exec::safePopen(const OW_Array<OW_String>& command,
 #endif
 
 	if (retval.pid() == -1)
-		return retval;
+		OW_THROW(OW_ExecErrorException, "fork() failed");
 
 	if (retval.pid() == 0)
 	{
@@ -367,6 +502,11 @@ int OW_PopenStreams::getExitStatus()
 	return m_impl->getExitStatus();
 }
 
+void OW_PopenStreams::setProcessStatus(int ps)
+{
+	m_impl->setProcessStatus(ps);
+}
+
 OW_PopenStreams::OW_PopenStreams(const OW_PopenStreams& src)
 	: m_impl(src.m_impl)
 {
@@ -377,3 +517,150 @@ OW_PopenStreams& OW_PopenStreams::operator=(const OW_PopenStreams& src)
 	m_impl = src.m_impl;
 	return *this;
 }
+
+
+void 
+OW_Exec::executeProcessAndGatherOutput(const OW_Array<OW_String>& command,
+	OW_String& output, int& processstatus,
+	int timeoutsecs, int outputlimit)
+{
+	processstatus = -1;
+	OW_PopenStreams streams(safePopen(command));
+
+	gatherOutput(output, streams, processstatus,
+		timeoutsecs, outputlimit);
+
+	if (processstatus == -1)
+	{
+		processstatus = streams.getExitStatus();
+	}
+}
+
+
+// if timeoutsecs < 0, no timeout will be used
+// if outputlimit < 0, no limit will be set for the bytes to read.
+// the processes status will be stored in processstatus.  That should be evaluated using the WIFEXITED() and WEXITSTATUS() macros.
+// returns ERROR or SUCCESS, TIMEOUT, BUFFERFULL
+void 
+OW_Exec::gatherOutput(OW_String& output, OW_PopenStreams& streams, int& processstatus, int timeoutsecs, int outputlimit)
+{
+	bool outIsOpen = true;
+	bool errIsOpen = true;
+	bool got_child_return_code = false;
+	int cumulative_timeout = 0;
+	while(outIsOpen || errIsOpen)
+	{
+		OW_SelectTypeArray fdset;
+
+		if (outIsOpen)
+		{
+			fdset.push_back(streams.out()->getSelectObj());
+		}
+		if (errIsOpen)
+		{
+			fdset.push_back(streams.err()->getSelectObj());
+		}
+		
+		// check if the child has exited
+		if (!got_child_return_code && streams.pid() != -1)
+		{
+			pid_t waitpidrv;
+			waitpidrv = waitpidNoINTR(streams.pid(), &processstatus, WNOHANG);
+			if (waitpidrv == -1)
+			{
+				OW_THROW(OW_ExecErrorException, format("waitpid failed errno = %1(%2)\n", errno, strerror(errno)).c_str());
+			}
+			else if (waitpidrv != 0)
+			{
+				got_child_return_code = 1;
+				streams.pid(-1);
+				streams.setProcessStatus(processstatus);
+			}
+		}
+
+		const int mstimeout = 100; // use 1/10 of a second
+		int selectrval = OW_Select::select(fdset, mstimeout);
+		switch (selectrval)
+		{
+			case OW_Select::OW_SELECT_INTERRUPTED:
+				// if we got interrupted, just try again
+				break;
+
+			case OW_Select::OW_SELECT_ERROR:
+			{
+				OW_THROW(OW_ExecErrorException, format("error selecting on stdout and stderr: %1(%2)", errno, strerror(errno)).c_str());
+			}
+			break;
+
+			case OW_Select::OW_SELECT_TIMEOUT:
+			{
+				if (got_child_return_code)
+				{
+					// pretend the child output is closed, since they've exited
+					// and select timed out.
+					outIsOpen = false;
+					errIsOpen = false;
+				}
+				else
+				{
+					++cumulative_timeout;
+					if (timeoutsecs >= 0 && cumulative_timeout >= (timeoutsecs * 10))
+					{
+						OW_THROW(OW_ExecTimeoutException, "timedout");
+					}
+				}
+			}
+			break;
+
+			default:
+			{
+				cumulative_timeout = 0;
+				OW_UnnamedPipeRef readstream;
+				// if both have output, we'll get error first.
+				if (streams.err()->getSelectObj() == fdset[selectrval])
+				{
+					readstream = streams.err();
+				}
+				else if (streams.out()->getSelectObj() == fdset[selectrval])
+				{
+					readstream = streams.out();
+				}
+				if (readstream)
+				{
+					char buff[1024];
+					int readrc = readstream->read(buff, sizeof(buff) - 1);
+					if (readrc == 0)
+					{
+						if (readstream == streams.out())
+							outIsOpen = false;
+						else
+							errIsOpen = false;
+					}
+					else if (readrc == -1)
+					{
+						OW_THROW(OW_ExecErrorException, format("Error reading stdout from lwcclient: %1(%2)", errno, strerror(errno)).c_str());
+					}
+					else
+					{
+						buff[readrc] = 0;
+						if (outputlimit >= 0 && output.length() + readrc > static_cast<size_t>(outputlimit))
+						{
+							// the process output too much, so just copy what we can and return error
+							int lentocopy = outputlimit - output.length();
+							if (lentocopy >= 0 && static_cast<size_t>(lentocopy) < sizeof(buff))
+							{
+								buff[lentocopy] = '\0';
+								output += buff;
+							}
+							OW_THROW(OW_ExecBufferFullException, "");
+						}
+						output += buff;
+					}
+				}
+			}
+			break;
+		}
+	}
+}
+
+
--- src/common/OW_Exec.hpp
+++ src/common/OW_Exec.hpp
@@ -36,40 +36,210 @@
 #include "OW_String.hpp"
 #include "OW_ArrayFwd.hpp"
 
+DECLARE_EXCEPTION(ExecTimeout);
+DECLARE_EXCEPTION(ExecBufferFull);
+DECLARE_EXCEPTION(ExecError);
+
 class OW_UnnamedPipe;
 typedef OW_Reference<OW_UnnamedPipe> OW_UnnamedPipeRef;
 
 class OW_PopenStreamsImpl;
+/**
+ * This class represents a connection to a process.
+ */
 class OW_PopenStreams
 {
-	public:
-		OW_PopenStreams();
-		~OW_PopenStreams();
-		OW_PopenStreams(const OW_PopenStreams& src);
-		OW_PopenStreams& operator=(const OW_PopenStreams& src);
+public:
+	OW_PopenStreams();
+	~OW_PopenStreams();
+	OW_PopenStreams(const OW_PopenStreams& src);
+	OW_PopenStreams& operator=(const OW_PopenStreams& src);
 
-		OW_UnnamedPipeRef in() const;
-		void in(OW_UnnamedPipeRef pipe);
-		OW_UnnamedPipeRef out() const;
-		void out(OW_UnnamedPipeRef pipe);
-		OW_UnnamedPipeRef err() const;
-		void err(OW_UnnamedPipeRef pipe);
-		pid_t pid() const;
-		void pid(pid_t newPid);
-		int getExitStatus();
+	/**
+	 * Get a write-only pipe to the process's stdin.
+	 */
+	OW_UnnamedPipeRef in() const;
+	
+	/**
+	 * Set a pipe to the process's stdin
+	 */
+	void in(OW_UnnamedPipeRef pipe);
 
-	private:
-		OW_Reference<OW_PopenStreamsImpl> m_impl;
+	/**
+	 * Get a read-only pipe to the process's stdout
+	 */
+	OW_UnnamedPipeRef out() const;
+
+	/**
+	 * Set a pipe to the process's stdout
+	 */
+	void out(OW_UnnamedPipeRef pipe);
+
+	/**
+	 * Get a read-only pipe to the process's stderr
+	 */
+	OW_UnnamedPipeRef err() const;
+
+	/**
+	 * Set a pipe to the process's stderr
+	 */
+	void err(OW_UnnamedPipeRef pipe);
+
+	/**
+	 * Get the process's pid.  If the process's exit status has already been
+	 * read by calling getExitStatus(), then this will return -1
+	 */
+	pid_t pid() const;
+
+	/**
+	 * Set the process's pid.  This is only usefule when constructing an 
+	 * instance of this class.
+	 */
+	void pid(pid_t newPid);
+
+	/**
+	 * Get the process's exit status.  
+	 * If the child process is still running, this function will do everything
+	 * possible to terminate it.
+	 * After calling this function, the pipes to will be closed, and this object
+	 * is basically useless.
+	 * @return The exit status of the process.  This should be evaluated using
+	 * the family of macros (WIFEXITED(), WEXITSTATUS(), etc.) from "sys/wait.h"
+	 */
+	int getExitStatus();
+
+	/**
+	 * Sets the process's exit status.
+	 * This function is used by OW_Exec::gatherOutput()
+	 */
+	void setProcessStatus(int ps);
+
+private:
+	OW_Reference<OW_PopenStreamsImpl> m_impl;
 };
 
 class OW_Exec
 {
-	public:
+public:
 
-		static int safeSystem(const OW_Array<OW_String>& command);
-		static OW_PopenStreams safePopen(const OW_Array<OW_String>& command,
-				const OW_String& initialInput = OW_String());
+	/**
+	 * Execute a command.
+	 * The command will inherit stdin, stdout, and stderr from the parent
+	 * process.  This function will not search the path for command[0], so
+	 * the absolute path to the binary should be specified.  If the path needs
+	 * to be searched, you can set command[0] = "/bin/sh"; command[1] = "-c";
+	 * and then fill in the rest of the array with the command you wish to 
+	 * execute.
+	 * This function blocks until the child process exits.  Be careful that
+	 * the command you run doesn't hang.  It is recommended to use
+	 * executeProcessAndGatherOutput() if the command is untrusted.
+	 *
+	 * @param command
+	 *  command[0] is the binary to be executed.
+	 *  command[1] .. command[n] are the command line parameters to the command.
+	 *
+	 * @return 127 if the execve() call for command[0]
+     * fails,  -1 if there was another error and the return code
+     * of the command otherwise.
+	 */
+	static int safeSystem(const OW_Array<OW_String>& command);
 
+	/**
+	 * Execute a command.
+	 * The command's stdin, stdout, and stderr will be connected via pipes to
+	 * the parent process that can be accessed from the return value.
+	 * This function will not search the path for command[0], so
+	 * the absolute path to the binary should be specified.  If the path needs
+	 * to be searched, you can set command[0] = "/bin/sh"; command[1] = "-c";
+	 * and then fill in the rest of the array with the command you wish to 
+	 * execute.
+	 * This function does *not* block until the child process exits.
+	 *
+	 * @param command
+	 *  command[0] is the binary to be executed.
+	 *  command[1] .. command[n] are the command line parameters to the command.
+	 *
+	 * @param initialInput
+	 *  The string is sent to stdin of the child process.
+	 *
+	 * @return A OW_PopenStreams object which can be used to access the child
+	 *  process and/or get it's return value.
+	 */
+	static OW_PopenStreams safePopen(const OW_Array<OW_String>& command,
+			const OW_String& initialInput = OW_String());
+
+	/**
+	 * Wait for output from a child process.  The function returns when the
+	 * process exits. In the case that the child process doesn't exit, if a 
+	 * timout is specified, then an OW_ExecTimeoutException is thrown.
+	 * If the process outputs more bytes than outputlimit, an 
+	 * OW_ExecBufferFullException is thrown.
+	 *
+	 * @param output An out parameter, the process output will be appended to 
+	 *  this string.
+	 * @param streams The connection to the child process.
+	 * @param processstatus An out parameter, which will contain the process
+	 *  status.  It is only valid if the funtion returns. In the case an 
+	 *  exception is thrown, it's undefined. It should be evaluated using the 
+	 *  family of macros (WIFEXITED(), WEXITSTATUS(), etc.) from "sys/wait.h"
+	 * @param timeoutsecs Specifies the number of seconds to wait for the 
+	 *  process to exit. If the process hasn't exited after timeoutsecs seconds,
+	 *  an OW_ExecTimeoutException will be thrown. If timeoutsecs < 0, the 
+	 *  timeout will be infinite, and no exception will ever be thrown.
+	 * @param outputlimit Specifies the maximum size of the parameter output,
+	 *  in order to constrain possible memory usage.  If the process outputs
+	 *  more data than will fit into output, then an OW_ExecBufferFullException
+	 *  is thrown. If outputlimit < 0, the limit will be infinite, and an
+	 *  OW_ExecBufferFullException will never be thrown.
+	 *
+	 * @throws OW_ProcessError on error. 
+	 * @throws OW_ProcessTimeout if the process hasn't finished within timeoutsecs. 
+	 * @throws OW_ProcessBufferFull if the process output exceeds outputlimit bytes.
+	 */
+	static void gatherOutput(OW_String& output, OW_PopenStreams& streams, int& processstatus, int timeoutsecs = -1, int outputlimit = -1);
+	
+	/**
+	 * Run a process, collect the output, and wait for it to exit.  The 
+	 * function returns when the
+	 * process exits. In the case that the child process doesn't exit, if a 
+	 * timout is specified, then an OW_ExecTimeoutException is thrown.
+	 * If the process outputs more bytes than outputlimit, an 
+	 * OW_ExecBufferFullException is thrown.
+	 * This function will not search the path for command[0], so
+	 * the absolute path to the binary should be specified.  If the path needs
+	 * to be searched, you can set command[0] = "/bin/sh"; command[1] = "-c";
+	 * and then fill in the rest of the array with the command you wish to 
+	 * execute.
+	 *
+	 * @param command
+	 *  command[0] is the binary to be executed.
+	 *  command[1] .. command[n] are the command line parameters to the command.
+	 * @param output An out parameter, the process output will be appended to 
+	 *  this string.
+	 * @param streams The connection to the child process.
+	 * @param processstatus An out parameter, which will contain the process
+	 *  status.  It is only valid if the funtion returns. In the case an 
+	 *  exception is thrown, it's undefined. It should be evaluated using the 
+	 *  family of macros (WIFEXITED(), WEXITSTATUS(), etc.) from "sys/wait.h"
+	 * @param timeoutsecs Specifies the number of seconds to wait for the 
+	 *  process to exit. If the process hasn't exited after timeoutsecs seconds,
+	 *  an OW_ExecTimeoutException will be thrown. If timeoutsecs < 0, the 
+	 *  timeout will be infinite, and no exception will ever be thrown.
+	 * @param outputlimit Specifies the maximum size of the parameter output,
+	 *  in order to constrain possible memory usage.  If the process outputs
+	 *  more data than will fit into output, then an OW_ExecBufferFullException
+	 *  is thrown. If outputlimit < 0, the limit will be infinite, and an
+	 *  OW_ExecBufferFullException will never be thrown.
+	 *
+	 * @throws OW_ProcessError on error. 
+	 * @throws OW_ProcessTimeout if the process hasn't finished within timeoutsecs. 
+	 * @throws OW_ProcessBufferFull if the process output exceeds outputlimit bytes.
+	 */
+	static void executeProcessAndGatherOutput(const OW_Array<OW_String>& command,
+		OW_String& output, int& processstatus,
+		int timeoutsecs = -1, int outputlimit = -1);
+	
+	
 };
 
 #endif
