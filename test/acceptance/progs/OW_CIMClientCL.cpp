e4695739c6538a98fc7c979b2a71f9294dcc9a06
Dan Nuffer <nuffer>
2004-01-25 04:34:33 +0000

    Changed namespace test to use sub1 instead of Vintela.

--- test/acceptance/progs/OW_CIMClientCL.cpp
+++ test/acceptance/progs/OW_CIMClientCL.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2001 Center 7, Inc All rights reserved.
+* Copyright (C) 2001 Vintela, Inc. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -11,14 +11,14 @@
 *    this list of conditions and the following disclaimer in the documentation
 *    and/or other materials provided with the distribution.
 *
-*  - Neither the name of Center 7 nor the names of its
+*  - Neither the name of Vintela, Inc. nor the names of its
 *    contributors may be used to endorse or promote products derived from this
 *    software without specific prior written permission.
 *
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL Center 7, Inc OR THE CONTRIBUTORS
+* ARE DISCLAIMED. IN NO EVENT SHALL Vintela, Inc. OR THE CONTRIBUTORS
 * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -1446,12 +1446,12 @@ createNameSpace(CIMClient& hdl)
 
 	try
 	{
-		hdl.createNameSpace("root/testsuite/Vintela");
-		hdl.createNameSpace("root/testsuite/Vintela/test");
+		hdl.createNameSpace("root/testsuite/sub1");
+		hdl.createNameSpace("root/testsuite/sub1/test");
 		// test case sensitivity.  Namespaces (unlike other CIM names)
 		// are *NOT* case sensitive, and so we should be able to create
-		// both vintela and Vintela
-		hdl.createNameSpace("root/testsuite/vintela");
+		// both sub1 and Sub1
+		hdl.createNameSpace("root/testsuite/Sub1");
 	}
 	catch (CIMException& e)
 	{
@@ -1499,9 +1499,9 @@ deleteNameSpace(CIMClient& hdl)
 
 	try
 	{
-		hdl.deleteNameSpace("root/testsuite/Vintela/test");
-		hdl.deleteNameSpace("root/testsuite/Vintela");
-		hdl.deleteNameSpace("root/testsuite/vintela");
+		hdl.deleteNameSpace("root/testsuite/sub1/test");
+		hdl.deleteNameSpace("root/testsuite/sub1");
+		hdl.deleteNameSpace("root/testsuite/Sub1");
 	}
 	catch (CIMException& e)
 	{
