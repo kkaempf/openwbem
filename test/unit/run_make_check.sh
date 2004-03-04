b9b176fcf70460918f2b7e1a1a37ae127ea73f8a
Dan Nuffer <nuffer>
2004-03-04 22:32:18 +0000

    Fixed for HPUX.

--- test/unit/run_make_check.sh
+++ test/unit/run_make_check.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 
+
 LD_LIBRARY_PATH="`pwd`/../../db/db:`pwd`/../../src/common:`pwd`/../../src/cimom/server:`pwd`/../../src/cimom/common:`pwd`/../../src/listener:`pwd`/../../src/services/http:`pwd`/../../src/http/common:`pwd`/../../src/http/client:`pwd`/../../src/client:`pwd`/../../src/xml:`pwd`/../../src/cimxmllistener:`pwd`/../../src/providerifcs/cpp:`pwd`/../../src/repositories/hdb:`pwd`/../../src/mof:$LD_LIBRARY_PATH" 
 
-LIBPATH="`pwd`/../../db/db:`pwd`/../../src/common:`pwd`/../../src/cimom/server:`pwd`/../../src/cimom/common:`pwd`/../../src/listener:`pwd`/../../src/services/http:`pwd`/../../src/http/common:`pwd`/../../src/http/client:`pwd`/../../src/client:`pwd`/../../src/xml:`pwd`/../../src/cimxmllistener:`pwd`/../../src/providerifcs/cpp:`pwd`/../../src/repositories/hdb:`pwd`/../../src/mof:$LIBPATH" 
+# AIX
+LIBPATH="$LD_LIBRARY_PATH:$LIBPATH" 
 
-export LD_LIBRARY_PATH LIBPATH
+# HP-UX
+SHLIB_PATH="$LD_LIBRARY_PATH:$SHLIB_PATH"
+
+export LD_LIBRARY_PATH LIBPATH SHLIB_PATH
 
 ./unitMain $@
 
