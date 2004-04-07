2b4d8321e40145a6079d088560b35060f9f5c971
Dan Nuffer <nuffer>
2004-04-07 22:30:14 +0000

    Fixed the test.

--- test/unit/OW_FormatTestCases.cpp
+++ test/unit/OW_FormatTestCases.cpp
@@ -61,9 +61,9 @@ void OW_FormatTestCases::testPercent()
 
 void OW_FormatTestCases::testError()
 {
-	unitAssert( Format("%1,%2xx", 1).toString() == "1,\n*** Parameter specifier too large.\n*** Error in Format string at \"2xx\".\n" );
-	unitAssert( Format("%xABC", 'e').toString() == "\n*** Error in Format string at \"xABC\".\n" );
-	unitAssert( Format("%0ABC", 'e').toString() == "\n*** Error in Format string at \"0ABC\".\n" );
+	unitAssert( Format("%1,%2xx", 1).toString() == "1,\n*** Parameter specifier too large.\n*** Error in format string at \"2xx\".\n" );
+	unitAssert( Format("%xABC", 'e').toString() == "\n*** Error in format string at \"xABC\".\n" );
+	unitAssert( Format("%0ABC", 'e').toString() == "\n*** Error in format string at \"0ABC\".\n" );
 }
 
 Test* OW_FormatTestCases::suite()
