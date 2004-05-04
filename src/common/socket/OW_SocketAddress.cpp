3112cc7218f0c69e1425eb6922be6c85b1e6d3ef
edge <edge>
2004-05-04 16:20:57 +0000

    build changes for OSX so I can get both bundles and dylibs where I need them. Also a fix in SocketAddress

--- src/common/socket/OW_SocketAddress.cpp
+++ src/common/socket/OW_SocketAddress.cpp
@@ -82,6 +82,10 @@ SocketAddress::getUDS(const String& filename)
 	rval.m_UDSNativeAddress.sun_len = filename.length() + 1;
 	rval.m_nativeSize = ::strlen(rval.m_UDSNativeAddress.sun_path) +
 		offsetof(struct sockaddr_un, sun_path) + 1;	
+#elif defined OW_DARWIN
+	rval.m_UDSNativeAddress.sun_len = filename.length() + 1;
+	rval.m_nativeSize = ::strlen(rval.m_UDSNativeAddress.sun_path) +
+		offsetof(struct sockaddr_un, sun_path) + 1;	
 #else
 	rval.m_nativeSize = sizeof(rval.m_UDSNativeAddress.sun_family) +
 		 ::strlen(rval.m_UDSNativeAddress.sun_path);
