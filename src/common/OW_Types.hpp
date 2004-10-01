013d3c96258458bed2e26205bda8ae5249752a64
Dan Nuffer <nuffer>
2004-10-01 22:14:57 +0000

    Adding a long and unsigned long constructor for platforms on which long doesnt map to Int32 or Int64.

--- src/common/OW_Types.hpp
+++ src/common/OW_Types.hpp
@@ -54,35 +54,45 @@ typedef signed char        			Int8;
 #if OW_SIZEOF_SHORT_INT == 2
 typedef unsigned short     			UInt16;
 typedef short       				Int16;
+#define OW_INT16_IS_SHORT 1
 #elif OW_SIZEOF_INT == 2
 typedef unsigned int     			UInt16;
 typedef int       					Int16;
+#define OW_INT16_IS_INT 1
 #endif
 #if OW_SIZEOF_INT == 4
 typedef unsigned int       			UInt32;
 typedef int         				Int32;
+#define OW_INT32_IS_INT 1
 #elif OW_SIZEOF_LONG_INT == 4
 typedef unsigned long       		UInt32;
 typedef long         				Int32;
+#define OW_INT32_IS_LONG 1
 #endif
 #if OW_SIZEOF_LONG_INT == 8
 typedef unsigned long   UInt64;
 typedef long    Int64;
+#define OW_INT64_IS_LONG 1
 #elif OW_SIZEOF_LONG_LONG_INT == 8
 typedef unsigned long long 			UInt64;
 typedef long long   				Int64;
+#define OW_INT64_IS_LONG_LONG 1
 #else
 #error "Compiler must support 64 bit long"
 #endif
 #if OW_SIZEOF_DOUBLE == 8
 typedef double						Real64;
+#define OW_REAL64_IS_DOUBLE 1
 #elif OW_SIZEOF_LONG_DOUBLE == 8
 typedef long double					Real64;
+#define OW_REAL64_IS_LONG_DOUBLE 1
 #endif
 #if OW_SIZEOF_FLOAT == 4
 typedef float						Real32;
+#define OW_REAL32_IS_FLOAT 1
 #elif OW_SIZEOF_DOUBLE == 4
 typedef double						Real32;
+#define OW_REAL32_IS_DOUBLE 1
 #endif
 typedef off_t	off_t;
 
