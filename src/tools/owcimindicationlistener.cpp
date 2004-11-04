e33b352b47476f0d0003763c992b8a1169284c72
Dan Nuffer <nuffer>
2004-11-04 00:53:21 +0000

    Setting logger level to E_FATAL_ERROR_LEVEL

--- src/tools/owcimindicationlistener.cpp
+++ src/tools/owcimindicationlistener.cpp
@@ -141,6 +141,7 @@ int main(int argc, char* argv[])
 		String query(argv[3]);
 		CIMListenerCallbackRef mcb(new myCallBack);
 		LoggerRef logger(new CerrLogger);
+		logger->setLogLevel(E_FATAL_ERROR_LEVEL);
 		HTTPXMLCIMListener hxcl(logger);
 		ClientAuthCBIFCRef getLoginInfo(new GetLoginInfo);
 		String handle = hxcl.registerForIndication(url, ns, query, "wql1", ns, mcb, getLoginInfo);
