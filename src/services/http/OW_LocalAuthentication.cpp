042e9f29ef9b78deab5c614f90737cd00f42d56a
Dan Nuffer <nuffer>
2007-02-13 21:20:49 +0000

    Use the real path for the auth dir, otherwise owlocalhelper wont delete the files.

--- src/services/http/OW_LocalAuthentication.cpp
+++ src/services/http/OW_LocalAuthentication.cpp
@@ -170,7 +170,7 @@ namespace
 //////////////////////////////////////////////////////////////////////////////
 LocalAuthentication::LocalAuthentication()
 	: m_logger(COMPONENT_NAME)
-	, m_local_auth_dir(ConfigOpts::installed_owdata_dir + "/OWLocal")
+	, m_local_auth_dir(FileSystem::Path::realPath(ConfigOpts::installed_owdata_dir + "/OWLocal"))
 {
 	if (useHelper())
 	{
